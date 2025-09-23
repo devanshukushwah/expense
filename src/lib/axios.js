24a64c6d8630c8f919615e863b9e2cb4b4c24118
Devanshu Kushwah <devanshukushwah.001@gmail.com>
2025-09-24 00:57:21 +0530

    fix: remove NEXT_PUBLIC_API_BASE_URL env

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,6 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api",
   withCredentials: true, // if you're using cookies
 });
 
